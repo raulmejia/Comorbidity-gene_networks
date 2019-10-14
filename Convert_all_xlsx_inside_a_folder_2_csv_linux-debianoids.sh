b8738611230a150125f82bbe07c889acd083228b
raulmejia <r8a8m8p8@gmail.com>
2019-10-13 22:43:45 -0500

    xls2csv

---
+++ Convert_all_xlsx_inside_a_folder_2_csv_linux-debianoids.sh
@@ -0,0 +1,5 @@
+sudo apt-get install xlsx2csv
+
+xlsx2csv -d "tab" ~/Documentos/Data_Comorbility_networks/TABLAS\ DE\ LOS\ 3\ NODOS\ SELECCIONADOS/TABLA\ DEL\ NODO\ I25.8/I25.8\ INFO\ DEL\ NODO/Z72.0\ Tobacco\ use.xlsx >  ~/test.csv
+
+xlsx2csv -d "tab" 
